0e4a20799baeb375e9eaf150b65d44e0561f5ed5
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-05-25 17:54:31 +0530

    database connecton message

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chat_app'
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => {})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
 .catch(err => console.error('MongoDB connection error:', err));
 
 // Auth Routes
